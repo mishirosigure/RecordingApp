760e8d59a015dbf4fb72281cad6aacdbbe5b43f1
mishiro_sigure <misirosigure@gmail.com>
2019-04-26 18:27:06 +0900

    sample音楽の再生・停止

--- app/src/main/java/com/example/recordingapp/MainActivity.kt
+++ app/src/main/java/com/example/recordingapp/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.recordingapp
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener {
+            startPlay()
+        }
+
+        stopButton.setOnClickListener {
+            stopPlay()
+        }
+    }
+
+    private var mp : MediaPlayer? = null
+
+    private fun startPlay(){
+        mp = MediaPlayer.create(this , R.raw.sample)
+        try {
+            mp?.start()
+        }catch (e : IOException){
+            e.printStackTrace()
+        }
+    }
+    private fun stopPlay(){
+        try {
+            mp?.stop()
+            mp?.prepare()
+            mp?.release()
+        }catch (e : IllegalStateException){
+            e.printStackTrace()
+        }catch (e : IOException){
+            e.printStackTrace()
+        }
     }
 }
