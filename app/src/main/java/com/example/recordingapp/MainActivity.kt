128504e5e91082298c5889c9e8f051b9ef7cd8ec
mishiro_sigure <misirosigure@gmail.com>
2019-04-27 17:55:33 +0900

    音声１つの録音・再生

--- app/src/main/java/com/example/recordingapp/MainActivity.kt
+++ app/src/main/java/com/example/recordingapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.recordingapp
 
 import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.io.IOException
+import java.lang.Exception
 import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startButton.setOnClickListener {
+        recordStartButton.setOnClickListener {
+            startMediaRecord()
+        }
+
+        recordStopButton.setOnClickListener {
+            stopMediaRecord()
+        }
+
+        playStartButton.setOnClickListener {
             startPlay()
         }
 
-        stopButton.setOnClickListener {
+        playStopButton.setOnClickListener {
             stopPlay()
         }
     }
@@ -25,8 +37,11 @@ class MainActivity : AppCompatActivity() {
     private var mp : MediaPlayer? = null
 
     private fun startPlay(){
-        mp = MediaPlayer.create(this , R.raw.sample)
+      //  mp = MediaPlayer.create(this , R.raw.sample)
         try {
+            mp = MediaPlayer()
+            mp?.setDataSource(filePath)
+            mp?.prepare()
             mp?.start()
         }catch (e : IOException){
             e.printStackTrace()
@@ -40,6 +55,42 @@ class MainActivity : AppCompatActivity() {
         }catch (e : IllegalStateException){
             e.printStackTrace()
         }catch (e : IOException){
+            e.printStackTrace()
+        }
+    }
+
+    /*
+    録音処理
+     */
+    private var mr : MediaRecorder? = null
+    private val filePath : String = "${Environment.getExternalStorageDirectory()}/sample.wav"
+
+    fun startMediaRecord(){
+        try {
+            var mediaFile : File? = File(filePath)
+            if(mediaFile!!.exists()){
+                mediaFile?.delete()
+            }
+            mediaFile = null
+
+            mr = MediaRecorder()
+            mr?.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mr?.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+            mr?.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+            mr?.setOutputFile(filePath)
+            mr?.prepare()
+            mr?.start()
+        }catch (e : Exception){
+            e.printStackTrace()
+        }
+    }
+
+    fun stopMediaRecord(){
+        try {
+            mr?.stop()
+            mr?.reset()
+            mr?.release()
+        }catch (e : Exception){
             e.printStackTrace()
         }
     }
